e93deb970d69a216e1b4f4df7bde801606caf330
aaravdulal0 <aaravdulal0@gmail.com>
2026-04-22 21:21:58 +0545

    Refactor Discord client intents and logging

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 console.log("FILE STARTED");
+
 require("dotenv").config();
 const { Client, GatewayIntentBits } = require("discord.js");
 
+console.log("DOTENV LOADED");
+
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-    GatewayIntentBits.GuildMembers
-  ]
+  intents: [GatewayIntentBits.Guilds]
 });
 
 client.once("ready", () => {
-  console.log(`${client.user.tag} is ONLINE 🚀`);
+  console.log("BOT IS ONLINE");
 });
 
-// Load systems
-require("./systems/chatCommands")(client);
-
-client.login(process.env.MTQ5MzkyNDcxODk0NzA3NDA5OA.GBuIvX.gXhQmuB0N9OhY1yWRrGCJdiL00bkDdPWhPkqp8);
+client.login(process.env.MTQ5MzkyNDcxODk0NzA3NDA5OA.GBuIvX.gXhQmuB0N9OhY1yWRrGCJdiL00bkDdPWhPkqp8
+            );
