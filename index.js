459600251566790a80e153a489838a4ef52698d8
aaravdulal0 <aaravdulal0@gmail.com>
2026-04-22 20:54:58 +0545

    Initialize Discord bot client in index.js
    Set up Discord bot client with necessary intents and login.

---
+++ index.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`${client.user.tag} is ONLINE 🚀`);
+});
+
+// Load systems
+require("./systems/chatCommands")(client);
+
+client.login(process.env.MTQ5MzkyNDcxODk0NzA3NDA5OA.GBuIvX.gXhQmuB0N9OhY1yWRrGCJdiL00bkDdPWhPkqp8);
